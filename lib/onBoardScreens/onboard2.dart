9a35bc9b92953a0f98b1f885b3a23a65be7238af
ericatsu <ekaatsu@st.ug.edu.gh>
2022-07-01 04:26:02 +0000

    onboard good

--- lib/onBoardScreens/onboard2.dart
+++ lib/onBoardScreens/onboard2.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -9,9 +11,51 @@ class onboard2 extends StatelessWidget {
     final double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: Center(
-        child: Lottie.asset('student.json'),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50,
+            right: 5,
+            left: 5,
+            child: Lottie.asset(
+              'student.json',
+              width: 1000,
+              alignment: Alignment.center,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                SizedBox(height: height * 0.32),
+                Container(
+                  margin: EdgeInsets.only(top: 300.0, bottom: 5.0),
+                  child: Text(
+                    'Something here',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Something here Something here Something here',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
--- lib/onBoardScreens/onboard3.dart
+++ lib/onBoardScreens/onboard3.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -9,12 +11,48 @@ class onboard3 extends StatelessWidget {
     final double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          child: Center(
-            child: Lottie.asset('searching.json'),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50,
+            right: 5,
+            left: 5,
+            child: Lottie.asset('searching.json',
+            width: 1000,
+            alignment: Alignment.center,
+            ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomCenter,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+
+                  SizedBox(height: height * 0.32),
+                  Container(
+                    margin: EdgeInsets.only(top: 300.0, bottom: 5.0),
+                    child: Text('Something here',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                     ),
+                    ),
+                  ),
+                  Text(
+                    'Something here Something here Something here',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+          ),
+        ],
       ),
     );
   }
